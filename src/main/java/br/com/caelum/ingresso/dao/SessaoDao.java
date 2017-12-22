bf2393d43590af5f07b44ebcb130ff7f6da9d172
Rafael <rafael_perracini@hotmail.com>
2017-12-21 22:43:36 -0200

    ajuste

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -21,12 +21,12 @@ public class SessaoDao {
 	}
 
 	public List<Sessao> buscaSessoesDaSala(Sala sala) {
-		return manager.createQuery("select	s	from	Sessao	s	where	s.sala	=	:sala", Sessao.class)
+		return manager.createQuery("select s from Sessao s where s.sala	= :sala", Sessao.class)
 				.setParameter("sala", sala).getResultList();
 	}
 
 	public List<Sessao> buscaSessoesDoFilme(Filme filme) {
-		return manager.createQuery("select	s	from	Sessao	s	where	s.filme	=	:filme", Sessao.class)
+		return manager.createQuery("select s from Sessao s	where s.filme =	:filme", Sessao.class)
 				.setParameter("filme", filme).getResultList();
 	}
 	
